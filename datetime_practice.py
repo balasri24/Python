7e419fa6daf2ce03722e083e8e9e72759cf3706c
Balasri Kalavalapalli <balasrikalavalapalli24@gmail.com>
2026-07-14 08:09:16 +0530

    Add files via upload

---
+++ datetime_practice.py
@@ -0,0 +1,16 @@
+import datetime 
+# current date and time
+now=datetime.datetime.now()
+print(now)
+# current date 
+today=datetime.date.today()
+print(today)
+#current time 
+time=datetime.datetime.now().time()
+print(time)
+#year ,month,day,separate ga print cheyadam
+today=datetime.date.today()
+print(today.year)
+print(today.month)
+print(today.day)
+
