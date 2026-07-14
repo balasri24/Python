30600fe712256c9834758e4b947147644ec2098c
Balasri Kalavalapalli <balasrikalavalapalli24@gmail.com>
2026-07-14 13:50:49 +0530

    Add files via upload

---
+++ os library.py
@@ -0,0 +1,21 @@
+import os 
+#getcwd ante c=current w= working d=directory or folder
+print(os.getcwd())
+print(os.listdir())
+files=os.listdir()
+print(files)
+print(len(files))
+#os.mkdir
+# mk=make(create)
+# dir=directory(folder)
+#os.mkdir("tharun")           #
+# projects ane oka new folder create avuthundhi
+# os.rename()
+#os.rename("python notes","AI projects")
+#os.rename("tharun","os library")     #
+if os.path.exists("projects"):
+ print("folder already exits")
+else:
+ print("folder does not exists")
+
+
